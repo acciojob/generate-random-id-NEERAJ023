3b67abe683a7e0415e4a291bc20b0b9aea0a8575
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-16 20:59:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function makeid(l) {
   // write your code here
+	
+	let b="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	
+	
+	let result="";
+	for(let i=0; i<l;i++){
+		let e=Math.floor(Math.random()*b.length);
+		result+=b.charAt(e);
+	}
+	return result;
 }
+
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
